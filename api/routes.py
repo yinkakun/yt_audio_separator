ae85dacb5c72aca7ff681167716fa1a6827ef112
Yinka Adedire <yinka.adedire@internetbrands.com>
2025-09-15 13:10:02 +0100

    add type hint for storage param

--- api/routes.py
+++ api/routes.py
@@ -3,11 +3,12 @@ from fastapi import FastAPI
 from api.factory import configure_middleware, create_base_app, create_lifespan_manager
 from api.handlers import register_error_handlers, register_routes
 from config.logger import get_logger
+from services.storage import CloudflareR2
 
 logger = get_logger(__name__)
 
 
-def create_fastapi_app(config, storage) -> FastAPI:
+def create_fastapi_app(config, storage: CloudflareR2) -> FastAPI:
     """Create and configure the FastAPI application"""
 
     app = create_base_app(config)
